9f3bec2e627debf141df2c1db29e1a50b122a112
ogammal <gammal93@mac.com>
2018-10-28 12:42:02 -0400

    correct answer check now working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,25 @@
 // Declaring the global variables needed
-var timer = 1000;
+var timer = 5;
 var correct = 0;
 var incorrect = 0;
-// var unanswered = 0;
+var unanswered = 0;
 var setInt;
 
 // Once the timer is done, stop and clear the timer. Initiate allDone function
 function timeUp() {
   clearInterval(setInt);
-  // if ($('input').is(':checked')) {
-  //   if (input.val() == true) {
-  //     correct++;
-  //   }
-  // }
-  // else {
-  //   incorrect++;
-  // }
+  for (var i = 0; i < 10; i++) {
+    var userAnswer = $("input[name='btn" + i + "']:checked").val();
+    if (userAnswer === "true") {
+      correct++;
+    }
+    else if(userAnswer==="false") {
+      incorrect++;
+    }
+    console.log(userAnswer);
+  }
+  unanswered = 10 - (correct + incorrect);
+
   $('#timer').empty()
   $('#quiz-area').empty()
   allDone();
@@ -28,8 +32,8 @@ function allDone() {
   $('#quiz-area').append(correctAnswer);
   var incorrectAnswer = $('<div>Incorrect: ' + incorrect + '</div>')
   $('#quiz-area').append(incorrectAnswer);
-  // var totalUnanswered = $('<div>Unanswered: ' + unanswered + '</div>')
-  // $('#quiz-area').append(totalUnanswered);
+  var totalUnanswered = $('<div>Unanswered: ' + unanswered + '</div>')
+  $('#quiz-area').append(totalUnanswered);
 };
 
 // reduce the count down timer every second and then call timeUp function when timer hits 0
@@ -67,7 +71,6 @@ $('#start').on("click", function() {
         $('#quiz-area').append(choices);
         var choiceText = $('<span>' + response[i].answers[j].text + '</span>')
         $('#quiz-area').append(choiceText);
-        console.log(choices.val())
         }
       }
   })  
