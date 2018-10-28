8583af01426eb0fa57a4308f9b4b4ad1541d4a65
ogammal <gammal93@mac.com>
2018-10-27 22:28:53 -0400

    css styles added, answer check still giving me a headache

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,21 @@
 // Declaring the global variables needed
-var timer = 60;
+var timer = 1000;
 var correct = 0;
 var incorrect = 0;
-var unanswered = 0;
+// var unanswered = 0;
 var setInt;
 
 // Once the timer is done, stop and clear the timer. Initiate allDone function
 function timeUp() {
   clearInterval(setInt);
+  // if ($('input').is(':checked')) {
+  //   if (input.val() == true) {
+  //     correct++;
+  //   }
+  // }
+  // else {
+  //   incorrect++;
+  // }
   $('#timer').empty()
   $('#quiz-area').empty()
   allDone();
@@ -20,8 +28,8 @@ function allDone() {
   $('#quiz-area').append(correctAnswer);
   var incorrectAnswer = $('<div>Incorrect: ' + incorrect + '</div>')
   $('#quiz-area').append(incorrectAnswer);
-  var totalUnanswered = $('<div>Unanswered: ' + unanswered + '</div>')
-  $('#quiz-area').append(totalUnanswered);
+  // var totalUnanswered = $('<div>Unanswered: ' + unanswered + '</div>')
+  // $('#quiz-area').append(totalUnanswered);
 };
 
 // reduce the count down timer every second and then call timeUp function when timer hits 0
@@ -52,16 +60,15 @@ $('#start').on("click", function() {
       for (var i = 0; i < questions.length; i++) {
         var question = $('<p>' + response[i].text + '<p>')
         $('#quiz-area').append(question);
-        var name = "btn" + i
+        var radioName = "btn" + i
         for (var j = 0; j < 4; j++) {
-        var choices = $('<input type="radio">').val(response[i].answers[j].text);
-        choices.attr("name", name);
+        var choices = $('<input id="check" type="radio">').val(response[i].answers[j].correct);
+        choices.attr("name", radioName);
         $('#quiz-area').append(choices);
         var choiceText = $('<span>' + response[i].answers[j].text + '</span>')
         $('#quiz-area').append(choiceText);
         console.log(choices.val())
         }
       }
-      // if ()
   })  
 });
